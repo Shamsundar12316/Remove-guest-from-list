fda9ba98b8191b30f07189b632e6bb4ce06889b4
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:43:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+// guest from list
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest_list = ["Ali", "Fahad", "Ahmed", "Lal", "kiran"];
+let not_coming = "Ali";
+let new_guest = "Raj";
+guest_list[0] = new_guest;
+guest_list.unshift("Alishba");
+guest_list.splice(guest_list.length / 2, 2, "Atif aslam");
+guest_list.push("Mavesh");
+console.log(`\n\nUnfortunately we are out of space, so have space for two guest only,\n\n`);
+while (guest_list.length > 2) {
+    let remove_guest = guest_list.pop();
+    console.log(`Dear ${remove_guest}, you are not invited for a dinner tomorrow`);
+}
+for (let i = 0; i < guest_list.length; i++) {
+    console.log(`Dear ${guest_list[i]}\n\nyou all are still invited for a dinner tomorrow,\n\nThank you,\n\n`);
+}
+guest_list.splice(0, 2);
+console.log(guest_list);
